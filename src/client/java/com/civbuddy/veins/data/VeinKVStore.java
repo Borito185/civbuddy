ebdf772e85700feab0fe9305331e61a6d7500f10
Boris Annink <boris.annink@gmail.com>
2026-01-17 11:20:41 +0100

    Fix bug where found diamonds wouldn't be tracked

--- src/client/java/com/civbuddy/veins/data/VeinKVStore.java
+++ src/client/java/com/civbuddy/veins/data/VeinKVStore.java
@@ -9,7 +9,6 @@ public final class VeinKVStore {
     private VeinKVStore() {}
 
     public final static String VEIN_NAME_KEY = "veins.active_vein_name";
-    public final static String DO_COUNT_DIA_KEY = "veins.do_count_dia";
 
     public static String getActiveVeinName() throws SQLException {
         return KeyValueDao.get(VEIN_NAME_KEY).orElse("default");
@@ -17,18 +16,6 @@ public final class VeinKVStore {
 
     public static void setActiveVeinName(String veinId) throws SQLException {
         KeyValueDao.put(VEIN_NAME_KEY, veinId);
-    }
-
-    public static boolean getDoCountDia() throws SQLException {
-        return KeyValueDao
-                .get(DO_COUNT_DIA_KEY)
-                .map(VeinKVStore::parseBool)
-                .orElse(false); // default
-    }
-
-    public static void setDoCountDia(boolean value) throws SQLException {
-        // store as "1"/"0" (SQLite-friendly, unambiguous)
-        KeyValueDao.put(DO_COUNT_DIA_KEY, value ? "1" : "0");
     }
 
     private static boolean parseBool(String v) {
