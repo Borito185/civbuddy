41e0bef49fcb9809c39cdd48cc76c1aa34e460b6
Spicon17 <spicon17@gmail.com>
2026-02-22 03:25:49 +0100

    made keybind change possible. also updated all lang files to reflect the new keybind category.

--- src/client/java/com/civbuddy/commands/CommandClient.java
+++ src/client/java/com/civbuddy/commands/CommandClient.java
@@ -6,10 +6,14 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 
 public class CommandClient {
+    private static final KeyMapping.Category CIVBUDDY_CATEGORY =
+        KeyMapping.Category.register(Identifier.fromNamespaceAndPath("civbuddy", "civbuddy"));
+
     public static void initialize() {
         registerKeybinding();
         // Commands are loaded on world join now (see CivBuddyClient JOIN handler)
@@ -20,7 +24,7 @@ public class CommandClient {
             "key.civbuddy.open_bookmarks",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_BACKSLASH,
-            KeyMapping.Category.MISC
+            CIVBUDDY_CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
